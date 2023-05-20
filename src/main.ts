c3ef5c7dddf3da4b7475ceafc8a09a0459563418
Frozn <frozndiscord@gmail.com>
2023-05-20 13:44:33 -0400

    fix: custom tooltip

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import App from './App.vue';
 
 const app = createApp(App);
 
-app.use(Tooltip)
+app.use(Tooltip, {
+  name: 'VueCustomTooltip',
+  background: '#1e293b',
+  fontWeight: 500
+})
 
 app.mount('#app');
